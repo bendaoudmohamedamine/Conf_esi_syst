3ef0bc46da907c8d7805b808a9bfc9deee114ffc
Khebchi Abdallah <a.khebchi@esi-sba.dz>
2020-03-27 23:47:30 +0100

    new commit

--- src/main/java/com/esi/conf_esi/entity/User.java
+++ src/main/java/com/esi/conf_esi/entity/User.java
@@ -1,4 +1,26 @@
 package com.esi.conf_esi.entity;
 
-public class User {
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity @Data
+@AllArgsConstructor @NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    private UUID id ;
+    private String nom ;
+    private String prenom ;
+    private String email ;
+    private String pwd ;
+
+
+
+
 }
